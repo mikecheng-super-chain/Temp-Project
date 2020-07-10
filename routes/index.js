da1d1031d7daa9f95cf21a243144051b44471ebc
MCCMike <mccmike@MCCMikede-MacBook-Air.local>
2020-07-10 11:52:28 +0800

    Initial Setup

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+//An isolated instance capable only of performing middleware and routing functions.
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.render('views/index')
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,40 @@
+//To check we are in devlopment environment or not.
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()//'.load()' ===> '.config()'.
+  }
+
+//express
+//https://expressjs.com/zh-tw/api.html
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const indexRouter = require('./routes/index')
+//MongoDB
+const mongoose = require('mongoose')
+const db = mongoose.connection
+
+//Specify 'ejs' as 'view-engine'.
+app.set('view engine', 'ejs')
+//Specify __dirname as 'views' with address '/views'.
+//__dirname returns the path of the folder where the current JavaScript file resides.
+app.set('views', __dirname, '/views')
+//Specify 'layouts/layout' as 'layout'.
+app.set('layout', 'views/layouts/layout')
+
+app.use(expressLayouts)
+//Serve static content for the app from the “public” directory.
+app.use(express.static('public'))
+app.use('/', indexRouter)
+
+//To listen connections from 'process.env.PORT' or port(3000).
+app.listen(process.env.PORT || 3000)
+
+//Connect to MongoDB
+//Use 'DATABASE_URL' to set the path in '.env' file. 
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true //?Can be delete?
+})
+//To check and show whether there have any connection error.
+db.on('error', error => console.error(error))
+//To check whether the server is connected to MongoDB.
+db.once('open', () => console.log('Connected to Mongoose'))
