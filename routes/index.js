aae07d8b3d194eef2970cf5c524b66b7cb0ec194
MCCMike <mccmike@MCCMikede-MacBook-Air.local>
2020-07-13 16:07:42 +0800

    Testing

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,8 @@ const express = require('express')
 //An isolated instance capable only of performing middleware and routing functions.
 const router = express.Router()
 
-/*
 router.get('/', (req, res) => {
-    res.render('views/index.ejs', { name: req.body.name })
+    res.render('views/index.ejs', { name: 'please login first' })
 })
-*/
 
 module.exports = router
